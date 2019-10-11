ea5eee286d3b146f63beb2aad2dec2e83a344c9b
lixu <461229187@qq.com>
2019-10-11 18:11:29 +0800

    feat: 🎸 每一项再次进行翻译

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,12 @@
 import {
-	window as vswindow,
 	commands,
 	ExtensionContext,
-	Range
 } from "vscode";
-import translate from './translate';
+import { translate } from './translate';
 
 export function activate(context: ExtensionContext) {
 	let disposable = commands.registerCommand('extension.XTranslator', () => {
-		const editor = vswindow.activeTextEditor;
-		if (!editor) {
-			vswindow.showInformationMessage('Open a file first to manipulate text selections');
-			return;
-		}
-		const selections = editor.selections;
-		const range = new Range(selections[0].start, selections[selections.length - 1].end);
-		const text = editor.document.getText(range) || '';
-		translate(text).then((res) => {
-			if(!('dict' in res)) {
-				vswindow.showInformationMessage('翻译失败！');
-			}
-			let data = <string[]>res.dict;
-			data = data.map(item => item.replace(/\[.*?\] /g,''));
-			if (data.length > 1) {
-				vswindow.showQuickPick(data, {
-					matchOnDescription: true
-				}).then((item) => {
-					editor.edit(edit => edit.replace(range, <string>item));
-				});
-			} else {
-				editor.edit(edit => edit.replace(range, data[0]));
-			}
-		});
+		translate();
 	});
 
 	context.subscriptions.push(disposable);
--- src/translate.ts
+++ src/translate.ts
@@ -1,5 +1,54 @@
+import {
+	window as vswindow,
+	Range,
+  QuickPickItem,
+} from "vscode";
 import { youdao } from 'translation.js';
 
-export default function (q: string) {
-	return youdao.translate(q);
+export interface TranslatorResult {
+  dict?: string[];
+}
+
+export const translate = () => {
+  const editor = vswindow.activeTextEditor;
+  if (!editor) {
+    vswindow.showInformationMessage('Open a file first to manipulate text selections');
+    return;
+  }
+  const selections = editor.selections;
+  const range = new Range(selections[0].start, selections[selections.length - 1].end);
+  const text = editor.document.getText(range) || '';
+  youdao.translate(text).then(async (res: TranslatorResult) => {
+    if(res.dict) {
+      let data: any[] = await processingData(res.dict);
+      if (data.length > 1) {
+        vswindow.showQuickPick(data, {
+          matchOnDescription: true
+        }).then((item: QuickPickItem) => {
+          editor.edit(edit => edit.replace(range, item.label));
+        });
+      } else {
+        editor.edit(edit => edit.replace(range, data[0].label));
+      }
+    } else {
+      vswindow.showInformationMessage('Translation failed!');
+    }
+  });
+};
+
+// 为每一项进行翻译
+async function processingData (data: string[]) {
+  const _data: QuickPickItem[] = data.map((item: string) => {
+    const label = item.replace(/\[.*?\] /g,'');
+    return {
+      label,
+      description: '',
+      detail: ''
+    };
+  });
+  const promises = _data.map(async(item) => {
+    await youdao.translate(item.label).then((res: TranslatorResult) => item.detail = res.dict ? res.dict.join('  |  ') : '');
+  });
+  await Promise.all(promises);
+  return _data;
 }
