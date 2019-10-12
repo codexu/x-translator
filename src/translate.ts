6e3e9fe574e03d8f419349d45e32af19380fc78a
lixu <461229187@qq.com>
2019-10-12 17:34:59 +0800

    refactor: 💡 优化代码

--- src/translate.ts
+++ src/translate.ts
@@ -6,9 +6,9 @@ import {
 import { google } from 'translation.js';
 
 interface GoogleTranslateResult {
-  raw: Array<Array<Array<Array<string>>>>;
-  dict?: Array<string>;
-  result?: Array<string>;
+  raw: string[][][][];
+  dict?: string[];
+  result?: string[];
 }
 
 export const translate = () => {
@@ -21,35 +21,37 @@ export const translate = () => {
   const range = new Range(selections[0].start, selections[selections.length - 1].end);
   const text = editor.document.getText(range) || '';
   google.translate(text).then(async (res: GoogleTranslateResult) => {
-    const translateData: string[] = [];
-    if (res.result ) {
-      translateData.push(res.result[0]);
-    }
-    if (res.raw[1]) {
-      res.raw[1].forEach((rawItem) => {
-        rawItem[1].forEach(item => {
-          if (item !== translateData[0]) {
-            translateData.push(item);
-          }
-        });
-      });
-    }
-    if (translateData.length === 0) {
-      vswindow.showInformationMessage('Translation failed!');
-    }
+    const translateData: string[] = ProcessingTranslationResults(res);
     twiceTranslate(translateData).then((twiceTranslateResult: QuickPickItem[]) => {
-      vswindow.showQuickPick(twiceTranslateResult, {
-        matchOnDescription: true
-      }).then((item: QuickPickItem | undefined) => {
-        if (item !== undefined) {
-          editor.edit(edit => edit.replace(range, item.label));
-        }
+      showQuickPick(twiceTranslateResult).then((item: QuickPickItem) => {
+        editor.edit(edit => edit.replace(range, item.label));
       });
     }).catch(() => {
       vswindow.showInformationMessage('Translation failed!');
     });
   });
 };
+
+// 处理请求结果数据
+function ProcessingTranslationResults (res: GoogleTranslateResult): string[] {
+  const TranslationResults: string[] = [];
+  if (res.result ) {
+    TranslationResults.push(res.result[0]);
+  }
+  if (res.raw[1]) {
+    res.raw[1].forEach((rawItem) => {
+      rawItem[1].forEach(item => {
+        if (item !== TranslationResults[0]) {
+          TranslationResults.push(item);
+        }
+      });
+    });
+  }
+  if (TranslationResults.length === 0) {
+    vswindow.showInformationMessage('Translation failed!');
+  }
+  return TranslationResults;
+}
 
 // 为每一项进行翻译
 async function twiceTranslate (data: string[]): Promise<QuickPickItem[]> {
@@ -64,4 +66,17 @@ async function twiceTranslate (data: string[]): Promise<QuickPickItem[]> {
   });
   await Promise.all(promises);
   return result;
+}
+
+// 翻译结果 展示 -> 快速选择栏
+function showQuickPick (twiceTranslateResult: QuickPickItem[]): Promise<QuickPickItem> {
+  return new Promise(resolve => {
+    vswindow.showQuickPick(twiceTranslateResult, {
+      matchOnDescription: true
+    }).then((item: QuickPickItem | undefined) => {
+      if (item !== undefined) {
+        resolve(item);
+      }
+    });
+  });
 }
