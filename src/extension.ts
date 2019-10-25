d426a5316741efb9e69b67b8230efb71bb76949a
lixu <461229187@qq.com>
2019-10-25 10:35:57 +0800

    fix: 🐛 name

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,6 @@ import { commands, ExtensionContext } from "vscode";
 import { translate } from './translate';
 
 export function activate(context: ExtensionContext) {
-	let disposable = commands.registerCommand('extension.XTranslator', () => translate());
+	let disposable = commands.registerCommand('extension.x-translator', () => translate());
 	context.subscriptions.push(disposable);
 }
